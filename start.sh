96cbafe0c6c62252f9016b67b0833aa1b9a870a4
Lucifer Morningstar <103210935+Lucifer86790@users.noreply.github.com>
2023-02-22 14:18:19 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,12 +1,12 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/Lucifer86790/Shortner_file.git /Shortner_file
+  git clone https://github.com/Lucifer86790/dash-bot.git /dash-bot
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
-  git clone $UPSTREAM_REPO /Shortner_file
+  git clone $UPSTREAM_REPO /dash-bot
 fi
-cd /Shortner_file
+cd /dash-bot
 pip3 install -U -r requirements.txt
 echo "Starting Shortner_file...."
 python3 bot.py
